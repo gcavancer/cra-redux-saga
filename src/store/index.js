5235331ce1e36cd488a041d840cecd3c2c21ab36
gcavancer <gc@gregcurtis.co.uk>
2020-06-17 09:11:48 +0100

    Updated to remove history warning.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'connected-react-router'
 import createSagaMiddleware from 'redux-saga';
-import createHistory from 'history/createBrowserHistory'
+import { createBrowserHistory } from 'history';
 import rootReducer from '../rootReducer'
 import rootSaga from '../sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-export const history = createHistory()
+export const history = createBrowserHistory();
 
 const initialState = {}
 const enhancers = []
